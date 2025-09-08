7af1d895c132db49d3ed26a46737a1f14ac265d8
koushik0329 <kkambham@islander.tamucc.edu>
2025-09-08 15:05:31 -0500

    Initial Commit

---
+++ ArticleList_Walmart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArticleList_Walmart
+//
+//  Created by Koushik Reddy Kambham on 9/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
