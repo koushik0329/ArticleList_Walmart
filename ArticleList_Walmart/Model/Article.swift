2371a062f08df5b163cf4617a1718f3b1aee9d50
koushik0329 <kkambham@islander.tamucc.edu>
2025-09-09 09:33:08 -0500

    Model converted to Decodable

--- ArticleList_Walmart/Model/Article.swift
+++ ArticleList_Walmart/Model/Article.swift
@@ -7,11 +7,11 @@
 
 //https://gist.githubusercontent.com/congsun/600f3ad57298f9dbc23fedf6b2b25450/raw/73d7a8ed589652339ae3646d0a595298ce5e72c2/newsFeed.json
 
-struct ArticleList : Codable {
+struct ArticleList : Decodable {
     var articles : [Article]?
 }
 
-struct Article: Codable {
+struct Article: Decodable {
     let author: String?
     let description: String?
     let urlToImage: String?
