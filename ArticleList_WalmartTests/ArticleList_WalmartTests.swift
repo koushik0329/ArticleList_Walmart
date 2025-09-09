283dbb45d2227326cf7dc6581877c76017a6c6f9
koushik0329 <kkambham@islander.tamucc.edu>
2025-09-09 09:59:04 -0500

    Added Test Cases

--- ArticleList_WalmartTests/ArticleList_WalmartTests.swift
+++ ArticleList_WalmartTests/ArticleList_WalmartTests.swift
@@ -9,28 +9,46 @@ import XCTest
 @testable import ArticleList_Walmart
 
 final class ArticleList_WalmartTests: XCTestCase {
+    
+    var objArticle: ArticleViewModel!
+    var objNetwork: NetworkManager!
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        objArticle = ArticleViewModel()
+        objNetwork = NetworkManager()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        objArticle = nil
+        objNetwork = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testArticleCount() {
+        XCTAssertEqual(objArticle.getArticleCount(), 0)
+        
+        let sampleArticle = Article(
+                    author: "Test Author",
+                    description: "Test Description",
+                    urlToImage: "",
+                    publishedAt: "2020-02-05T15:47:40Z",
+                )
+        objArticle.articles.append(sampleArticle)
+        XCTAssertEqual(objArticle.getArticleCount(), 1)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testFirstArticleProperties() {
+        
+        let sampleArticle = Article(
+                    author: "Test Author",
+                    description: "Test Description",
+                    urlToImage: "",
+                    publishedAt: "2020-02-05T15:47:40Z",
+                )
+        objArticle.articles.append(sampleArticle)
+        
+        XCTAssertEqual(objArticle.getArticle(at: 0)?.author, "Test Author")
+        XCTAssertEqual(objArticle.getArticle(at: 0)?.description, "Test Description")
     }
-
 }
